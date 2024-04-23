a22beafc62c262a7c84d5331a0cb036709bf314c
Sindhiyahub <sindhiya.lcttintern@gmail.com>
2024-04-23 12:40:43 +0530

    Css

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,217 +1,3 @@
-// import React from "react";
-
-// import "./Product.css";
-// import "./WishList.css"
-// import ProductShare from './Productshare';
-
-
-// import { useState, useEffect } from 'react';
-// import { useLocation, useNavigate, Navigate } from "react-router-dom";
-
-// import { fetchCategoryData } from "../../../src/database"; // Import the database function
-// import { db, auth, provider } from '../../../src/firebase'; // Import your Firebase Firestore instance
-// import { collection, getDocs } from "firebase/firestore/lite";
-// import { doc, updateDoc, getDoc, setDoc } from 'firebase/firestore/lite';
-// import { actionTypes } from "../../store/AuthContext/reducer";
-// import { useStateValue } from "../../store/AuthContext/authContext";
-// import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-
-// import { CiHeart } from "react-icons/ci";
-// import WishList from "../../components/Header/Wishlist/WishList";
-
-// const Product = ({ user }) => {
-//   const [category, setCategory] = useState([]);
-//   const location = useLocation();
-//   const categoryName = location.state?.category;
-//   const navigate = useNavigate();
-//   console.log("category name from Mainshops",categoryName)
-//   console.log("user", user)
-
-//   const productName = "";
-//   const productUrl = "";
-//   const productPrice="";
-
-//   var displayName;
-//   var uid;
-
-//   auth.onAuthStateChanged(function (user) {
-//     if (user) {
-//       // User is signed in.
-//       displayName = user.displayName;
-//       uid = user.uid;
-//       console.log('User Name: ' + displayName);
-//       console.log('User ID: ' + uid);
-//     }
-//   });
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const newData = await fetchCategoryData("Products"); // Call the database function
-//       setCategory(newData);
-//     };
-
-//     fetchData();
-//     console.log(category)
-//   }, [categoryName]);
-
-//   const CategorizedProducts = category.filter(item => item.category == categoryName);
-
-//   console.log("category_name ", categoryName)
-
-//   const addToWishlist = async (product) => {
-//     console.log("product", product)
-//     // check if product already added in wishlist
-//     console.log(product.id)
-//     const docRef = doc(collection(db, "wishlist " + uid), product.id);
-//     const docSnap = await getDoc(docRef);
-//     await setDoc(docRef, {
-//       title: product.title,
-//       description: product.description,
-//       price: product.price,
-//       url: product.url,
-//     });
-//     console.log("Added to wishlist");
-
-
-//   };
-
-
-//   return (
-//     <><div className="product">
-//       {CategorizedProducts.map(item => (
-//         <div className="product__link" key={item.id}>
-//           <div className="product__container">
-//             <img className="product__image" src={item.url} alt="" />
-//             <div className="product__info">
-//               <h3>{item.title}</h3>
-//               <h3 className="product__price">${item.price}</h3>
-//               <button onClick={() => user ? addToWishlist(item) : navigate('/login', {state: { from: location } })}><FavoriteBorderOutlinedIcon className="heart-icon" /></button>
-//               <ProductShare className="sha" productName={item.title} productUrl={item.url} productPrice={item.price} />
-
-//             </div>
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//     </>
-//   );
-// };
-
-// export default Product;
-
-
-
-// import React from "react";
-
-// import "./Product.css";
-// import "./WishList.css"
-
-// import { useState, useEffect } from 'react';
-// import { useLocation, useNavigate, Navigate } from "react-router-dom";
-
-// import { fetchCategoryData } from "../../../src/database"; // Import the database function
-// import { db, auth, provider } from '../../../src/firebase'; // Import your Firebase Firestore instance
-// import { collection, getDocs } from "firebase/firestore/lite";
-// import { doc, updateDoc, getDoc, setDoc } from 'firebase/firestore/lite';
-// import { actionTypes } from "../../store/AuthContext/reducer";
-// import { useStateValue } from "../../store/AuthContext/authContext";
-// import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-
-// import { CiHeart } from "react-icons/ci";
-// import WishList from "../../components/Header/Wishlist/WishList";
-
-// const Product = ({ user }) => {
-//   const [category, setCategory] = useState([]);
-//   const location = useLocation();
-//   const categoryName = location.state?.category;
-//   const navigate = useNavigate();
-//   console.log("category name from Mainshops ", categoryName)
-//   console.log("user", user)
-
-//   var displayName;
-//   var uid;
-
-//   auth.onAuthStateChanged(function (user) {
-//     if (user) {
-//       // User is signed in.
-//       displayName = user.displayName;
-//       uid = user.uid;
-//       console.log('User Name: ' + displayName);
-//       console.log('User ID: ' + uid);
-//     }
-//   });
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const newData = await fetchCategoryData("Products"); // Call the database function
-//       setCategory(newData);
-//     };
-
-//     fetchData();
-//     console.log(category)
-//   }, [categoryName]);
-//   const CategorizedProducts = category.filter(item => item.category == categoryName);
-
-//   console.log("category_name ", categoryName)
-
-//   const addToWishlist = async (product) => {
-//     console.log("product", product)
-//     // check if product already added in wishlis
-//     console.log(product.id)
-//     const docRef = doc(collection(db, "wishlist " + uid), product.id);
-//     const docSnap = await getDoc(docRef);
-//     await setDoc(docRef, {
-//       title: product.title,
-//       description: product.description,
-//       colour: product.colour,
-//       fabric: product.fabric,
-//       price: product.price,
-//       url: product.url,
-//     });
-//     console.log("Added to wishlist");
-
-
-
-
-
-//     // if (!docSnap.exists()) {
-//     //   // wishlist does not exist for this user, add new document with generated id
-//     //   await setDoc(docRef, {
-//     //     description: product.description
-//     //   });
-//     //   console.log("Added to wishlist");
-//     // } else {
-//     //   console.log("Wishlist already exists");
-//     // }
-//   };
-
-//   return (
-//     <><div className="product">
-//       {CategorizedProducts.map(item => (
-//         <div className="product__link" key={item.id}>
-//           <div className="product__container">
-//             <img className="product__image" src={item.url} alt="" />
-//             <div className="product__info">
-//               <h3>{item.title}</h3>
-//               <h3 className="product__description">Description: {item.description}</h3>
-//               <h3 className="colour">Colour: {item.colour}</h3>
-//               <h3 className="fabric">Fabric: {item.fabric}</h3>
-//               {/* <h3 className="size">Available Size: {item.size}</h3> */}
-//               <h3 className="product__price">Price: ${item.price}</h3>
-//               <h3 className="shop">Shop: {item.shop}</h3>
-
-//               <button onClick={() => user ? addToWishlist(item) : navigate('/login', { state: { from: location } })}><FavoriteBorderOutlinedIcon className="heart-icon" /></button>
-//             </div>
-//           </div>
-//         </div>
-//       ))}
-//     </div >
-//     </>
-//   );
-// };
-
-// export default Product;
-
 import React, { useState, useEffect } from "react";
 import "./Product.css";
 import "./WishList.css";
@@ -320,36 +106,40 @@ const Product = ({ user }) => {
     <>
 
      {/* Sort by price button */}
-     <button onClick={sortByPriceHandler}>
-        Sort by Price {sortByPrice === "asc" ? "(Low to High)" : "(High to Low)"}
-      </button>
-      <div className="filter">
-        <h3>Filter by Shop:</h3>
-        <ul>
-          {shops.map((shop) => (
-            <li
-              key={shop}
-              onClick={() => filterByShop(shop)}
-              className={selectedShop === shop ? "selected" : ""}
-            >
-              {shop}
-            </li>
-          ))}
-        </ul>
-        <h3>By Colour : </h3>
-        <ul>
-          {colours.map((colour) => (
-            <li
-              key={colour}
-              onClick={() => filterByColour(colour)}
-              className={selectedColour === colour ? "selected" : ""}
-            >
-              {colour}
-            </li>
-          ))}
-        </ul>
-      </div>
-
+     <div className="sortfil">
+      <div className="sortlh1">
+      <h3>Sort</h3>
+      By Price:<button className="sortlh" onClick={sortByPriceHandler}>
+          {sortByPrice === "asc" ? "(Low to High)" : "(High to Low)"}
+        </button>
+        </div>
+        <div className="filterbyshop">
+        <h3>Filter</h3>
+          By Shop:<ul>
+            {shops.map((shop) => (
+              <li
+                key={shop}
+                onClick={() => filterByShop(shop)}
+                className={selectedShop === shop ? "selected" : ""}
+              >
+                {shop}
+              </li>
+            ))}
+          </ul>
+          By Colour:
+          <ul>
+            {colours.map((colour) => (
+              <li
+                key={colour}
+                onClick={() => filterByColour(colour)}
+                className={selectedColour === colour ? "selected" : ""}
+              >
+                {colour}
+              </li>
+            ))}
+          </ul>
+          </div>
+        </div> 
       <div className="product">
         {CategorizedProducts.filter(
           (item) =>
@@ -362,7 +152,7 @@ const Product = ({ user }) => {
               <div className="product__info">
                 <h3>{item.title}</h3>
                 <h3 className="product__description">
-                  Description: {item.description}
+                  {item.description}
                 </h3>
                 <h3 className="colour">Colour: {item.colour}</h3>
                 <h3 className="fabric">Fabric: {item.fabric}</h3>
